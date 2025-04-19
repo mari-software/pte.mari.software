dc83b14af57cd7edbd2f5dc31705d121aeca9b6a
Zarni Hlawn <zarnihlawn@outlook.com>
2025-04-20 02:16:13 +0630

    planning layout of index page

---
+++ src/lib/component/global/AppNavBar.svelte
@@ -0,0 +1,31 @@
+<script>
+	import BurgerMenuSvg from '$lib/asset/image/svg/burger-menu-svg.svelte';
+</script>
+
+<div class="navbar bg-base-100 shadow-sm">
+	<div class="navbar-start">
+		<div class="dropdown">
+			<div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+				<div class="h-5 w-5">
+					<svelte:component this={BurgerMenuSvg} />
+				</div>
+			</div>
+			<ul
+				class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+			>
+				<li><a href="/app/website-dashboard/about-website">About Us</a></li>
+			</ul>
+		</div>
+		<a href="/" class="btn btn-ghost text-xl">pte.mari.software</a>
+	</div>
+	<div class="navbar-center hidden lg:flex">
+		<ul class="menu menu-horizontal px-1">
+			<li><a href="/app/website-dashboard/about-website">about</a></li>
+		</ul>
+	</div>
+	<div class="navbar-end">
+		<a href="/app/website-dashboard/donate-website" class="btn btn-primary"
+			>donate</a
+		>
+	</div>
+</div>
--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -1,17 +1,7 @@
 <script lang="ts">
+	import HeroBanner from '$lib/component/local/app/index/HeroBanner.svelte';
+	import SoftwareCategory from '$lib/component/local/app/index/SoftwareCategory.svelte';
 </script>
 
-<article class="prose lg:prose-xl">
-	<h1>Garlic bread with cheese: What the science tells us</h1>
-	<p>
-		For years parents have espoused the health benefits of eating garlic bread
-		with cheese to their children, with the food earning such an iconic status
-		in our culture that kids will often dress up as warm, cheesy loaf for
-		Halloween.
-	</p>
-	<p>
-		But a recent study shows that the celebrated appetizer may be linked to a
-		series of rabies cases springing up around the country.
-	</p>
-	<!-- ... -->
-</article>
+<HeroBanner />
+<SoftwareCategory />
